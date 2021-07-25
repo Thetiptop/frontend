fe8be1981c4f6eef45dd0a5267babb48e7cdae43
christaime <linusague@gmail.com>
2021-07-25 10:51:00 +0200

    Profile Navs-tabs

--- src/app/views/pages/profil/profil.component.ts
+++ src/app/views/pages/profil/profil.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.component.scss']
 })
 export class ProfilComponent implements OnInit {
+  activeUrl: any;
+  links = [
+    { title: 'Historique', fragment: 'cgu' },
+    { title: 'Mes informations', fragment: 'mentions-legales' },
+    { title: 'Réclamer', fragment: 'confidentiality' },
+  ];
 
   constructor() { }
 
