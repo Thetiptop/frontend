991b0e0755ca1ded333fd7109666d6977681ff53
christaime <linusague@gmail.com>
2021-07-25 10:42:31 +0200

    A Propos Styling

--- src/app/views/pages/about/about.module.ts
+++ src/app/views/pages/about/about.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     children: [
       {
         path : '',
-        redirectTo: 'confidentiality',
+        redirectTo: 'cgu',
         pathMatch: 'full',
       },
       {
