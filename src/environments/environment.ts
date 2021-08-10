b297815126cfc84389166de001aec8c586e20c5c
christaime <linusague@gmail.com>
2021-08-10 15:52:18 +0200

    Facebook login config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,9 @@ export const environment = {
   apiURL: 'https://preprod-admin.dsp-archiwebo20-mt-ma-ca-fd.fr/api',
   loginApiURL : 'https://preprod-admin.dsp-archiwebo20-mt-ma-ca-fd.fr/api/login',
   registerApiUrl: 'https://preprod-admin.dsp-archiwebo20-mt-ma-ca-fd.fr/api/register',
-  cookieDomain: 'http://127.0.0.1:8000'
+  cookieDomain: 'http://127.0.0.1:8000',
+  googleIss: 'accounts.google.com',
+  googleIss2: 'https://accounts.google.com'
 };
 
 /*
