2cb1fb0b37e1a69ebcc33aeaf2a1a60f0bd904f0
christaime <linusague@gmail.com>
2021-08-12 18:51:53 +0200

    Home Styling

--- src/app/views/pages/home/home.component.ts
+++ src/app/views/pages/home/home.component.ts
@@ -18,14 +18,12 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
 
-
       const x = timeUntil('2021-09-13T20:20:20');
       console.log(`${x.years}:${x.days % 365}:${x.hours % 24}:${x.minutes % 60}:${x.seconds % 60}`);
       this.days = x.days;
       this.hours = x.hours;
       this.minutes = x.hours;
       this.seconds = x.seconds;
-
 
   }
 }
