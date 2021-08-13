b6489b97a7bb0684baedbe5a798edc6d7e674e98
christaime <linusague@gmail.com>
2021-08-13 09:32:58 +0200

    Home 'Jouer' bitton redirection if not signed in

--- src/app/views/pages/home/home.component.ts
+++ src/app/views/pages/home/home.component.ts
@@ -1,29 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { ChangeDetectionStrategy } from '@angular/core';
 import { timeUntil } from '@tobynatooor/countdown';
+import {Router} from '@angular/router';
+import {AuthStateService} from '../../../core/authentification/auth-state.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
+  isSignedIn: any;
   x: any;
   days: number;
   hours: number;
   seconds: number;
   minutes: number;
-  constructor() { }
+
+  constructor(
+    private router: Router,
+    private authState: AuthStateService,
+  ) { }
 
   ngOnInit(): void {
 
-      const x = timeUntil('2021-09-13T20:20:20');
-      console.log(`${x.years}:${x.days % 365}:${x.hours % 24}:${x.minutes % 60}:${x.seconds % 60}`);
-      this.days = x.days;
-      this.hours = x.hours;
-      this.minutes = x.hours;
-      this.seconds = x.seconds;
+    this.authState.userAuthState.subscribe(val => {
+      this.isSignedIn = val;
+    });
 
+    const x = timeUntil('2021-09-13T20:20:20');
+    console.log(`${x.years}:${x.days % 365}:${x.hours % 24}:${x.minutes % 60}:${x.seconds % 60}`);
+    this.days = x.days;
+    this.hours = x.hours;
+    this.minutes = x.hours;
+    this.seconds = x.seconds;
+
+  }
+
+  redirect(): void {
+    if (this.isSignedIn){
+      this.router.navigate(['/play']);
+    } else {
+      this.router.navigate(['/login']);
+
+    }
   }
 }
