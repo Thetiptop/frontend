537188d06928fc8dab724b47beb02304f337a533
christaime <linusague@gmail.com>
2021-08-17 17:31:40 +0200

    Canonical URL service and OG: tags

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,8 @@ export const environment = {
   registerApiUrl: 'https://preprod-admin.dsp-archiwebo20-mt-ma-ca-fd.fr/api/register',
   cookieDomain: 'http://127.0.0.1:8000',
   googleIss: 'accounts.google.com',
-  googleIss2: 'https://accounts.google.com'
+  googleIss2: 'https://accounts.google.com',
+  title: 'ThéTipTop',
 };
 
 /*
