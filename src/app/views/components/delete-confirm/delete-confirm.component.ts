812bcd959b9c59805a07129a80105e2b335ee9ad
Mihaela <m2timis@gmail.com>
2021-08-30 17:18:04 +0200

    delete account - service

--- src/app/views/components/delete-confirm/delete-confirm.component.ts
+++ src/app/views/components/delete-confirm/delete-confirm.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment'
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NotificationComponent } from '../../components/notification/notification.component';
 
 @Component({
   selector: 'app-delete-confirm',
@@ -8,11 +11,46 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 })
 export class DeleteConfirmComponent implements OnInit {
 
+  protected baseUrl: string = environment.apiURL;
+  popUpMessage: string;
+  errors: any;
+  UserId: any;
+  success: any;
+  
   constructor(
-    public activeModal: NgbActiveModal
+    public activeModal: NgbActiveModal,
+    private http: HttpClient,
+    private modalService: NgbModal
   ) { }
 
   ngOnInit(): void {
+  }
+
+  open(): any {
+    const modalRef = this.modalService.open(NotificationComponent, { centered: true });
+    modalRef.componentInstance.message = this.popUpMessage;
+  }
+
+  deleteAccount(): void {
+    this.UserId = JSON.parse(localStorage.getItem("client_id")).value;
+         
+    this.http.delete(this.baseUrl + '/user/delete/' + this.UserId).subscribe(
+      result=>{
+        this.success = result;
+        this.popUpMessage = 'Compte supprimé';
+        this.activeModal.close('Close click');
+      },
+  
+      error=>{
+        console.log(error)
+        this.errors = error.error.message || "Une erreur s'est produite. Veuillez contacter l'administrateur.";
+        this.popUpMessage='Erreur';
+      },
+  
+      ()=>{
+        this.open();
+      }
+      )
   }
 
 }
