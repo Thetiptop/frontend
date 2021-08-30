1bda87072262605a0a631aed8d547f95ca9d93f6
Mihaela <m2timis@gmail.com>
2021-08-30 17:28:49 +0200

    suppression log

--- src/app/views/pages/profil/historique/historique.component.ts
+++ src/app/views/pages/profil/historique/historique.component.ts
@@ -110,7 +110,6 @@ export class HistoriqueComponent implements OnInit {
     this.historiqueService.historique().subscribe(
       data => {
         this.historique = data.historical;
-        console.log(this.historique);
         this.dtTrigger.next();
       }
     );
